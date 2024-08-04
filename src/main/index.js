92b6cdb30463211825bb95dcfae2cc3ca927780d
coderDJing <149645781+coderDJing@users.noreply.github.com>
2024-08-04 18:15:26 +0800

    完成

---
+++ src/main/index.js
@@ -0,0 +1,177 @@
+import { app, shell, BrowserWindow, ipcMain, Menu, dialog } from 'electron'
+import { join } from 'path'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import icon from '../../resources/icon.png?asset'
+const fs = require('fs').promises;
+const path = require('path');
+const NodeID3 = require('node-id3')
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    title: 'setSongCoverTool',
+    width: 500,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    resizable: false,
+    ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false
+    }
+  })
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
+  // HMR for renderer base on electron-vite cli.
+  // Load the remote URL for development or the local html file for production.
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+
+  ipcMain.handle('select-SourceMp3', async (e) => {
+    const result = await dialog.showOpenDialog({
+      properties: ['openFile'],
+      filters: [{ name: 'MP3 Files', extensions: ['mp3'] }]
+    })
+    if (result.canceled) {
+      return null
+    }
+    const tags = NodeID3.read(result.filePaths[0])
+    if (tags.image) {
+      return { filePath: result.filePaths[0], imgData: tags.image }
+    } else {
+      return 'readFailed'
+    }
+  })
+
+  ipcMain.handle('select-TargetMp3Folder', async (e) => {
+    const result = await dialog.showOpenDialog({
+      properties: ['openDirectory', 'multiSelections']
+    })
+    if (result.canceled) {
+      return null
+    }
+    return result.filePaths
+  })
+  ipcMain.handle('select-TargetMp3Files', async (e) => {
+    const result = await dialog.showOpenDialog({
+      properties: ['openFile', 'multiSelections'],
+      filters: [{ name: 'MP3 Files', extensions: ['mp3'] }]
+    })
+    if (result.canceled) {
+      return null
+    }
+    return result.filePaths
+  })
+
+  async function findMP3Files(directory) {
+    let mp3Files = [];
+    async function traverseDirectory(dir) {
+      try {
+        const entries = await fs.readdir(dir, { withFileTypes: true });
+
+        for (let entry of entries) {
+          const fullPath = path.join(dir, entry.name);
+
+          if (entry.isDirectory()) {
+            // 递归遍历子目录
+            await traverseDirectory(fullPath);
+          } else if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.mp3') {
+            // 如果是MP3文件，则添加到结果数组中
+            mp3Files.push(fullPath);
+          }
+        }
+      } catch (err) {
+      }
+    }
+
+    await traverseDirectory(directory);
+    return mp3Files;
+  }
+  ipcMain.on('alert', async (e, type, title, message, detail) => {
+    dialog.showMessageBox({
+      type, // 可以是 'none', 'info', 'error', 'warning', 'question'
+      title, // 这里设置对话框的标题
+      message,
+      detail
+    });
+
+  })
+  ipcMain.on('replaceCover', async (e, mode, sourcePath, targetPaths) => {
+    // 源MP3文件路径
+    const sourceMp3Path = sourcePath
+    // 目标MP3文件路径
+    let targetMp3Paths = []
+    const tags = NodeID3.read(sourceMp3Path)
+
+    const replaceTags = {
+      image: tags.image
+    }
+    if (mode === 'folder') {
+      for (let dir of targetPaths) {
+        targetMp3Paths = targetMp3Paths.concat(await findMP3Files(dir));
+      }
+    } else {
+      targetMp3Paths = targetPaths
+    }
+    let index = 1
+    for (let item of targetMp3Paths) {
+      try {
+        NodeID3.update(replaceTags, item)
+      } catch (error) {
+
+      } finally {
+        index++
+        mainWindow.webContents.send('progressBar', index, targetMp3Paths.length)
+      }
+    }
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  Menu.setApplicationMenu(null)
+  // Set app user model id for windows
+  electronApp.setAppUserModelId('com.electron')
+
+  // Default open or close DevTools by F12 in development
+  // and ignore CommandOrControl + R in production.
+  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
+
+  // IPC test
+  ipcMain.on('ping', () => console.log('pong'))
+
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
---
+++ src/renderer/src/App.vue
@@ -0,0 +1,189 @@
+<script setup>
+import downIcon from "./assets/downIcon.png"
+import { ref, nextTick } from 'vue'
+
+const sourceMp3Path = ref('')
+let selectDialogShow = false
+const blobUrl = ref('')
+const selectSourceMp3 = async () => {
+  if (selectDialogShow) {
+    return
+  }
+  selectDialogShow = true
+  const result = await window.electron.ipcRenderer.invoke('select-SourceMp3')
+  selectDialogShow = false
+  if (result === null) {
+    return
+  }
+  if (result === 'readFailed') {
+    window.electron.ipcRenderer.send('alert', 'info', '提示', '选择的MP3文件没有封面，请选择有封面的MP3文件', '')
+    return
+  }
+  sourceMp3Path.value = result.filePath
+  let blob = new Blob([Uint8Array.from(result.imgData.imageBuffer)], { type: result.imgData.mime })
+  URL.revokeObjectURL(blobUrl.value)
+  blobUrl.value = URL.createObjectURL(blob)
+
+}
+const selectFolders = ref([])
+const mode = ref('')
+const selectTargetMp3Folder = async () => {
+  if (selectDialogShow) {
+    return
+  }
+  selectDialogShow = true
+  const result = await window.electron.ipcRenderer.invoke('select-TargetMp3Folder')
+  selectDialogShow = false
+  if (result === null) {
+    return
+  }
+  selectFolders.value = result
+  mode.value = 'folder'
+}
+const selectMp3Files = ref([])
+const selectTargetMp3Files = async () => {
+  if (selectDialogShow) {
+    return
+  }
+  selectDialogShow = true
+  const result = await window.electron.ipcRenderer.invoke('select-TargetMp3Files')
+  selectDialogShow = false
+  if (result === null) {
+    return
+  }
+  selectMp3Files.value = result
+  mode.value = 'file'
+}
+const loading = ref(false)
+const launch = () => {
+  if (!sourceMp3Path.value) {
+    window.electron.ipcRenderer.send('alert', 'info', '提示', '请选择要提取封面的MP3文件', '')
+    return
+  }
+  if (!mode.value) {
+    window.electron.ipcRenderer.send('alert', 'info', '提示', '请选择要替换的文件夹或MP3文件', '')
+    return
+  }
+  loading.value = true
+  window.electron.ipcRenderer.send('replaceCover',
+    mode.value + '',
+    sourceMp3Path.value + '',
+    JSON.parse(JSON.stringify(mode.value === 'folder' ? selectFolders.value : selectMp3Files.value))
+  )
+}
+const barNowNum = ref(0)
+const barTotal = ref(1)
+window.electron.ipcRenderer.on('progressBar', (event, nowNum, total) => {
+  barNowNum.value = nowNum
+  barTotal.value = total
+  if (nowNum == total) {
+    loading.value = false
+    barNowNum.value = 0
+    barTotal.value = 1
+    nextTick(() => {
+      window.electron.ipcRenderer.send('alert', 'none', '完成', '搞定了！', '')
+    })
+  }
+})
+
+</script>
+
+<template>
+  <div v-show="loading"
+    style="width: 100vw; height: 100vh;position: absolute;z-index: 999;top: 0;left: 0;background-color: rgba(0,0,0,0.8);display: flex;justify-content: center;align-items: center;">
+    <div style="font-size: 20px;">
+      <div class="container">
+        <div class="progress">
+          <div class="progress-bar" :style="'width:' + (barNowNum / barTotal) * 100 + '%'"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="mainBody" style="width: 100vw; height: 100vh">
+    <div class="functionArea" style="flex-direction: column; align-items: center">
+      <div style="width: 400px">
+        <div style="display: flex; justify-content: center">
+          <div class="button" @click="selectSourceMp3()">选择MP3提取封面</div>
+        </div>
+        <div v-if="blobUrl" style="display: flex; justify-content: center; padding-top: 20px">
+          <img :src="blobUrl" style="width: 150px; height: 150px" :dragable="false" />
+        </div>
+      </div>
+      <div style="display: flex; justify-content: center; padding-top: 10px">
+        <img :src="downIcon" style="width: 30px" />
+      </div>
+    </div>
+    <div class="functionArea" style="flex-direction: column; align-items: center">
+      <div style="display: flex; justify-content: space-between; width: 400px">
+        <div class="button" @click="selectTargetMp3Folder()">选择MP3文件夹</div>
+        <div style="line-height: 50px; font-size: 18px">
+          <span>或者</span>
+        </div>
+        <div class="button" @click="selectTargetMp3Files()">选择MP3文件</div>
+      </div>
+      <div style="font-size: 12px; padding-top: 10px">
+        *选择文件夹将会扫描文件夹及其子文件夹下的所有MP3文件<br />
+        *两种操作都可以多选
+      </div>
+      <div v-if="mode === 'folder'" style="padding-top: 10px">
+        选择了 {{ selectFolders[0]
+        }}{{ selectFolders.length > 1 ? ' 等' + selectFolders.length + '个' : ' ' }}文件夹
+      </div>
+      <div v-if="mode === 'file'" style="padding-top: 10px">
+        选择了{{ selectMp3Files[0]
+        }}{{ selectMp3Files.length > 1 ? ' 等' + selectMp3Files.length + '个' : ' ' }}MP3文件
+      </div>
+    </div>
+    <div class="functionArea" style="flex-direction: column; align-items: center; border-bottom: 0">
+      <div style="width: 400px">
+        <div style="display: flex; justify-content: center">
+          <div class="button" @click="launch()">开始替换</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div style="position: absolute;bottom: 10px;right: 5px;width: 120px;height: 20px;font-size: 12px;color: #cccccc;">
+    Author : CoderDJing</div>
+</template>
+<style lang="scss" scoped>
+.container {
+  width: 300px;
+  background-color: #cccccc;
+}
+
+.progress {
+  height: 20px;
+  display: flex;
+  align-items: center;
+}
+
+.progress-bar {
+  height: 20px;
+  background-color: #0078d4;
+}
+
+.functionArea {
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #42a5f5;
+}
+
+.mainBody {
+  color: #e3f2fd;
+}
+
+.button {
+  width: 150px;
+  height: 50px;
+  line-height: 50px;
+  background-color: #1565c0;
+  text-align: center;
+  border-radius: 10px;
+
+  &:hover {
+    background-color: #0d47a1;
+  }
+}
+</style>
